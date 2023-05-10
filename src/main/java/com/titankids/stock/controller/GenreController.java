771577f290e7f699ca3c03e4e8c895d402fa6665
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2023-05-10 20:38:19 +0500

    Добавлены методы для изменения/удаления жанров.

--- src/main/java/com/titankids/stock/controller/GenreController.java
+++ src/main/java/com/titankids/stock/controller/GenreController.java
@@ -6,8 +6,7 @@ import com.titankids.stock.repository.GenreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +21,25 @@ public class GenreController {
     public String getAllGenres(Model model) {
         addAttributes(model, genreRepository.findAll());
         return "genre-list";
+    }
+
+    @GetMapping(path = "/{id}")
+    public String getGenreById(@PathVariable Long id, Model model) {
+        model.addAttribute("genre", genreRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(String.format("Genre with id %d not found", id))));
+        return "genre";
+    }
+
+    @PostMapping()
+    public String updateGenre(@ModelAttribute Genre genre, Model model) {
+        model.addAttribute("genre", genreRepository.save(genre));
+        return "redirect:/genre/list";
+    }
+
+    @GetMapping(path = "/delete/{id}")
+    public String deleteById(@PathVariable Long id, Model model) {
+        genreRepository.deleteById(id);
+        return "redirect:/genre/list";
     }
 
     private void addAttributes(Model model, List<Genre> genres) {
