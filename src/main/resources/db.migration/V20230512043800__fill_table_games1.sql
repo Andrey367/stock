0276a11028fc7d5597053d5d22fe41591cb80595
Блинов Андрей Java <71576891+Andrey367@users.noreply.github.com>
2023-05-12 04:48:10 +0500

    Ошибка в синтаксисе(Исправлено)

--- src/main/resources/db.migration/V20230512043800__fill_table_games1.sql
+++ src/main/resources/db.migration/V20230512043800__fill_table_games1.sql
@@ -2,7 +2,7 @@ INSERT INTO games (name, description)
 VALUES
 ('Ведьмак 3: Дикая Охота','The Witcher 3: Wild Hunt — мультиплатформенная компьютерная ролевая игра с открытым миром, разработанная польской студией CD Projekt RED.'),
 ('The Last of Us Part II','The Last of Us Part II — компьютерная игра в жанре survival horror с элементами action и adventure, разработанная американской студией Naughty Dog.'),
-('Red Dead Redemption 2' 'Red Dead Redemption 2 — мультиплатформенная видеоигра, разработанная студией Rockstar Games.'),
+('Red Dead Redemption 2', 'Red Dead Redemption 2 — мультиплатформенная видеоигра, разработанная студией Rockstar Games.'),
 ('Assassin''s Creed: Вальгалла' ,'Assassin''s Creed Valhalla — мультиплатформенная компьютерная игра в жанре Action/RPG, разработанная студией Ubisoft Montreal.'),
 ('Cyberpunk 2077','Cyberpunk 2077 — мультиплатформенная компьютерная игра в жанре action/RPG, разработанная польской студией CD Projekt RED.'),
 ('Horizon Zero Dawn','Horizon Zero Dawn — компьютерная игра в жанре Action/RPG, разработанная нидерландской студией Guerrilla Games'),
