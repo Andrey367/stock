b7c6174c4750f30e53928fc0c2462b6d8f2e6c80
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2023-05-19 11:25:46 +0500

    Добавлены скрипты на наполнение данными таблиц

---
+++ src/main/resources/db.migration/V20230519105435__update_table_games.sql
@@ -0,0 +1,30 @@
+UPDATE games
+SET genre_id = 20 WHERE id = 2;
+UPDATE games
+SET genre_id = 2 WHERE id = 3;
+UPDATE games
+SET genre_id = 3 WHERE id = 4;
+UPDATE games
+SET genre_id = 3 WHERE id = 5;
+UPDATE games
+SET genre_id = 1 WHERE id = 6;
+UPDATE games
+SET genre_id = 1 WHERE id = 7;
+UPDATE games
+SET genre_id = 2 WHERE id = 8;
+UPDATE games
+SET genre_id = 1 WHERE id = 9;
+UPDATE games
+SET genre_id = 3 WHERE id = 10;
+UPDATE games
+SET genre_id = 2 WHERE id = 11;
+UPDATE games
+SET genre_id = 1 WHERE id = 12;
+UPDATE games
+SET genre_id = 3 WHERE id = 13;
+UPDATE games
+SET genre_id = 6 WHERE id = 14;
+UPDATE games
+SET genre_id = 1 WHERE id = 15;
+UPDATE games
+SET genre_id = 7 WHERE id = 16;
