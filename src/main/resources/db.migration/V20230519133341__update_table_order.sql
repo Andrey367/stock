ebf6113df6a3f09c42d37631a6fb6f317f5bd23f
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2023-05-19 14:09:09 +0500

    Обновлены данные по заказам

---
+++ src/main/resources/db.migration/V20230519133341__update_table_order.sql
@@ -0,0 +1,105 @@
+UPDATE "order" o
+SET price = 1499 * o.amount
+WHERE game_id = 52;
+UPDATE "order" o
+SET price = 2999 * o.amount
+WHERE game_id = 21;
+UPDATE "order" o
+SET price = 999 * o.amount
+WHERE game_id = 56;
+UPDATE "order" o
+SET price = 599 * o.amount
+WHERE game_id = 35;
+UPDATE "order" o
+SET price = 4500 * o.amount
+WHERE game_id = 1;
+UPDATE "order" o
+SET price = 1799 * o.amount
+WHERE game_id = 45;
+UPDATE "order" o
+SET price = 3000 * o.amount
+WHERE game_id = 16;
+UPDATE "order" o
+SET price = 4500 * o.amount
+WHERE game_id = 12;
+UPDATE "order" o
+SET price = 599 * o.amount
+WHERE game_id = 27;
+UPDATE "order" o
+SET price = 4500 * o.amount
+WHERE game_id = 7;
+UPDATE "order" o
+SET price = 2499 * o.amount
+WHERE game_id = 20;
+UPDATE "order" o
+SET price = 1499 * o.amount
+WHERE game_id = 30;
+UPDATE "order" o
+SET price = 559 * o.amount
+WHERE game_id = 54;
+UPDATE "order" o
+SET price = 3611 * o.amount
+WHERE game_id = 42;
+UPDATE "order" o
+SET price = 3799 * o.amount
+WHERE game_id = 37;
+UPDATE "order" o
+SET price = 3099 * o.amount
+WHERE game_id = 24;
+UPDATE "order" o
+SET price = 3000 * o.amount
+WHERE game_id = 3;
+UPDATE "order" o
+SET price = 3799 * o.amount
+WHERE game_id = 22;
+UPDATE "order" o
+SET price = 799 * o.amount
+WHERE game_id = 18;
+UPDATE "order" o
+SET price = 1399 * o.amount
+WHERE game_id = 50;
+UPDATE "order" o
+SET price = 1149 * o.amount
+WHERE game_id = 43;
+UPDATE "order" o
+SET price = 1999 * o.amount
+WHERE game_id = 58;
+UPDATE "order" o
+SET price = 3500 * o.amount
+WHERE game_id = 6;
+UPDATE "order" o
+SET price = 1999 * o.amount
+WHERE game_id = 29;
+UPDATE "order" o
+SET price = 650 * o.amount
+WHERE game_id = 47;
+UPDATE "order" o
+SET price = 3000 * o.amount
+WHERE game_id = 14;
+UPDATE "order" o
+SET price = 1999 * o.amount
+WHERE game_id = 19;
+UPDATE "order" o
+SET price = 1799 * o.amount
+WHERE game_id = 36;
+UPDATE "order" o
+SET price = 3500 * o.amount
+WHERE game_id = 10;
+UPDATE "order" o
+SET price = 500 * o.amount
+WHERE game_id = 39;
+UPDATE "order" o
+SET price = 999 * o.amount
+WHERE game_id = 17;
+UPDATE "order" o
+SET price = 3000 * o.amount
+WHERE game_id = 8;
+UPDATE "order" o
+SET price = 3500 * o.amount
+WHERE game_id = 2;
+UPDATE "order" o
+SET price = 3099 * o.amount
+WHERE game_id = 55;
+UPDATE "order" o
+SET price = 3499 * o.amount
+WHERE game_id = 46;
