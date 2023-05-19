34174a28dd2fc576dfa1d663c73b3778dc27b8b3
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2023-05-19 16:58:02 +0500

    Добавлен скрипт создания представления для отчёта. Добавлена ссылка на страницу в каталоге

---
+++ src/main/resources/db.migration/V20230519165647__create_view_buy_sales_report.sql
@@ -0,0 +1,12 @@
+drop view buy_sales_report;
+create view buy_sales_report as
+
+select row_number() over (order by month) id, month, sum(t.buys) buy, sum(t.sales) sale from
+    (
+
+        select case when o.order_type_id = 1 then (o.price * o.amount) else 0 end as buys,
+               case when o.order_type_id = 2 then (o.price * o.amount) else 0 end as sales,
+               date_part('Month', o.created_at) as month
+        from "order" o
+    ) t
+group by t.month
